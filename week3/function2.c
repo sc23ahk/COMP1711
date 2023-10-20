0253b88306b2dd8de1b454183fba15ca93cf5670
sc23ahk <146234966+sc23ahk@users.noreply.github.com>
2023-10-20 12:42:57 +0000

    week3 update

--- week3/function2.c
+++ week3/function2.c
@@ -3,13 +3,18 @@
 float sum (float a, float b);
 
 int main(){
-    int y = 2;
+    float y = 2.5841;
     float z = sum (5, y); // calling our new function
-    printf("The sum of 5 and %d is %f\n", y, z);
+    printf("The sum of 5 and %.3f is %.2f\n", y, z);
+    
+    
+    
+    
+    return 0;
 }
 
 
-float sum (int a, int b){
+float sum (float a, float b){
     float answer;
     answer = a + b;
     return answer;
